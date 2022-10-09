c62c80d2f2d78b67b57ddc8ec08b1678c0beb7ee
Jakub Hoczek <hoczek.jakub@gmail.com>
2022-10-09 22:27:04 +0200

    xd

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,27 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import AdminPanel from "./components/admin-panel";
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import AdminPanel from "./components/admin-panel";
+import LoginPanel from "./components/login-panel";
+
+interface IUser {
+    login: string
+    password: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <div className="login-container mx-5">
-        <div className="container">
-          <div className="title-container col-12 mx-1">
-            <h1>E-Cyber Security</h1>
-          </div>
-          <div className="form col-12">
-            <form>
-              <div className="input-container">
-                <label>Username </label>
-                <input type="text" name="uname" required />
-                {/*{renderErrorMessage("uname")}*/}
-              </div>
-              <div className="input-container">
-                <label>Password </label>
-                <input type="password" name="pass" required />
-                {/*{renderErrorMessage("pass")}*/}
-              </div>
-              <div className="button-container">
-                <input type="submit" />
-              </div>
-            </form>
-          </div>
+
+    const [user, setUser] = useState<IUser>();
+
+    function loginVerification(userModel: IUser) {
+
+    }
+
+    return (
+        <div className="App">
+            <LoginPanel userModel={user}/>
+            <AdminPanel userSettings={"xd"} />
         </div>
-        <AdminPanel userSettings={"xd"}/>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
