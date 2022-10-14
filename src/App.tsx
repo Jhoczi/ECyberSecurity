ed097a4382575046ab84d9317664112cd76e079b
Jakub Hoczek <hoczek.jakub@gmail.com>
2022-10-14 11:35:53 +0200

    Feat: Add front application in functional tsx components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,152 @@
-import React, { useEffect, useState } from 'react';
+import {Component, useEffect, useState} from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import './App.css';
-import AdminPanel from "./components/admin-panel";
-import LoginPanel from "./components/login-panel";
+import AuthService from "./services/auth.service";
 
-interface IUser {
-    login: string
-    password: string
+import {Login} from "./components/login-component";
+import {Register} from "./components/register-component";
+import {Home} from "./components/home-component";
+import {Profile} from "./components/profile-component";
+import {BoardUser} from "./components/board-user-component";
+import {BoardModerator} from "./components/board-moderator-component";
+import {BoardAdmin} from "./components/board-admin-component";
+
+import AuthVerify from "./common/AuthVerify";
+
+import EventBus from "./common/event-bus";
+import IUser from "./types/user-type";
+
+type Props = {};
+
+type State = {
+    showModeratorBoard: boolean,
+    showAdminBoard: boolean,
+    currentUser: IUser | undefined
 }
+
+
 
 function App() {
 
-    const [user, setUser] = useState<IUser>();
+    const [state, setState] = useState<State>({
+        showModeratorBoard: false,
+        showAdminBoard: false,
+        currentUser: undefined
+    });
 
+    useEffect(() => {
+        const user = AuthService.getCurrentUser();
 
-    function loginVerification(userModel: IUser) {
+        if (user) {
+            setState({
+                currentUser: user,
+                showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
+                showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+            });
+        }
 
+        EventBus.on("logout", logOut);
+
+        return () => {
+            EventBus.remove("logout", logOut);
+        };
+    });
+
+    const logOut = () => {
+        AuthService.logout();
+        setState({
+            showModeratorBoard: false,
+            showAdminBoard: false,
+            currentUser: undefined,
+        });
     }
 
+    const { currentUser, showModeratorBoard, showAdminBoard } = state;
+
     return (
-        <div className="App">
-            <LoginPanel userModel={user}/>
-            <AdminPanel userSettings={""} />
+        <div>
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <Link to={"/"} className="navbar-brand">
+                    bezKoder
+                </Link>
+                <div className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link to={"/home"} className="nav-link">
+                            Home
+                        </Link>
+                    </li>
+
+                    {showModeratorBoard && (
+                        <li className="nav-item">
+                            <Link to={"/mod"} className="nav-link">
+                                Moderator Board
+                            </Link>
+                        </li>
+                    )}
+
+                    {showAdminBoard && (
+                        <li className="nav-item">
+                            <Link to={"/admin"} className="nav-link">
+                                Admin Board
+                            </Link>
+                        </li>
+                    )}
+
+                    {currentUser && (
+                        <li className="nav-item">
+                            <Link to={"/user"} className="nav-link">
+                                User
+                            </Link>
+                        </li>
+                    )}
+                </div>
+
+                {currentUser ? (
+                    <div className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link to={"/profile"} className="nav-link">
+                                {currentUser.username}
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/login" className="nav-link" onClick={logOut}>
+                                LogOut
+                            </a>
+                        </li>
+                    </div>
+                ) : (
+                    <div className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link to={"/login"} className="nav-link">
+                                Login
+                            </Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link to={"/register"} className="nav-link">
+                                Sign Up
+                            </Link>
+                        </li>
+                    </div>
+                )}
+            </nav>
+
+            <div className="container mt-3">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/user" element={<BoardUser />} />
+                    <Route path="/mod" element={<BoardModerator />} />
+                    <Route path="/admin" element={<BoardAdmin />} />
+                </Routes>
+            </div>
+
+            <AuthVerify logOut={logOut}/>
         </div>
     );
 }
---
+++ src/components/board-moderator-component.tsx
@@ -0,0 +1,45 @@
+import {useEffect, useState} from "react";
+
+import UserService from "../services/user.service";
+
+type Props = {};
+
+type State = {
+    content: string;
+}
+
+export const BoardModerator = (props: Props) => {
+
+    const [state, setState] = useState<State>({
+        content: ""
+    });
+
+    useEffect(() => {
+        UserService.getModeratorBoard().then(
+            response => {
+                setState({
+                    content: response.data
+                });
+            },
+            error => {
+                setState({
+                    content: (
+                            error.response &&
+                            error.response.data &&
+                            error.response.data.message
+                        ) ||
+                        error.message ||
+                        error.toString()
+                });
+            }
+        );
+    });
+
+    return (
+        <div className="container">
+            <header className="jumbotron">
+                <h3>{state.content}</h3>
+            </header>
+        </div>
+    );
+}
